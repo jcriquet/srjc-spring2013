dc73d92cf3c7750ee12582efced4c6b7cab5d3e2
Jay Peretz <jay@mytowncontractor.com>
2013-02-17 17:46:16 -0800

    update weekly lesson number

--- spring2013/html5/index.php
+++ spring2013/html5/index.php
@@ -16,7 +16,7 @@ require('../authenticate.php');
 <script type="text/javascript">
 var user = '<?php print $_SESSION['user']['email'] ?>';
 var syllabus_id = 1;
-var currentLesson = 3;
+var currentLesson = 4;
 </script>
 <script src="../js/spring2013.js"></script>
 </head>
