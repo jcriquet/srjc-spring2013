9ece3fb7edd8be98a496ab007dd60638b99df748
Jay Peretz <jay@mytowncontractor.com>
2013-03-16 16:11:04 -0700

    allow deleting of wiki term definition

--- spring2013/wiki/wiki.js
+++ spring2013/wiki/wiki.js
@@ -11,13 +11,29 @@ $(document).ready(function() {
 			   content += '<dt>'+value.term+'</dt>';
 			   if (value.definition) {
 	               $.each(value.definition,function(index2,value2) {
-			         content += '<dd>'+value2.definition+'&mdash;<small>'+value2.first_name+'</small></dd>';
+					   if (value2.email === user) {
+			         		content += '<dd>'+value2.definition+'&mdash;<small>'+value2.first_name+'</small>&nbsp;<a class="btn btn-mini remove-definition" data-id="'+value2.definition_id+'"><i class="icon-remove"></i></a></dd>';
+					   } else {
+						   content += '<dd>'+value2.definition+'&mdash;<small>'+value2.first_name+'</small></dd>';
+					   }
 			       });
 			   };
 		   });
 		    content += '</dl>';
 			$("#glossary").html(content);
+			$("a.remove-definition").click(function() {
+				var definitionID = $(this).attr("data-id");
+				$.ajax({
+					  type: "POST",
+					  url: "remove-definition.php", 
+					  data: { definition: definitionID }
+					}).done(function(data) {
+						alert("Your definition was deleted.");
+						getwiki();
+					});
+	});
 	    });
+	
 	}
 	getwiki();
 	$("#submit-definition").click(function() {
