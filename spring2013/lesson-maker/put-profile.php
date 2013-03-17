aa428ccce35dedf11aae459454f07501a38f7039
Jay Peretz <jay@mytowncontractor.com>
2013-03-17 10:27:27 -0700

    fixed update bug in profile

--- spring2013/lesson-maker/put-profile.php
+++ spring2013/lesson-maker/put-profile.php
@@ -5,10 +5,10 @@ if ($newcontent['email']) {
   $profileupdate = $mysqli->prepare("update student set gallery_URL = ? , github_userid = ? where email = ?");
   $profileupdate->bind_param('sss',$newcontent['gallery_URL'],$newcontent['github_userid'],$newcontent['email']);
   $profileupdate->execute();
-  if ($newcontent['project_description']) {
   $projectupdate = $mysqli->prepare("update class set project_description = ? where student_email = ? and syllabus_syllabus_id = ?");
   $projectupdate->bind_param('sss',$newcontent['project_description'],$newcontent['email'],$newcontent['syllabus_id']);
   $projectupdate->execute();
-  }
-  $mysqli->close();
+  $profileupdate->close();
+  $projectupdate->close();
 } 
+$mysqli->close();
